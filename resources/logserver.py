be169b9506af097e10a6868a7a56472ca3e858cc
Carlos Novo <carlos.novo.negrillo@gmail.com>
2025-02-23 12:52:33 +0100

    basic flask log server example

---
+++ resources/logserver.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from flask import Flask, Markup, render_template
+
+import logging
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+logfile = "/esp8266.log"
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+     return "I'm alive!"
+
+@app.route('/log/<string:msg>')
+def log(msg):
+    print("received: %s", msg)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    msg = f"{timestamp} {msg}"
+
+    with open(logfile, "a") as f:
+        f.write(f"{msg}\n")
+
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888, threaded=True)
+
